f5b9f1d4239c02b866faa526a58dc034145394e5
Daniel Palmer <danp1995mail@gmail.com>
2020-06-14 21:39:01 +1000

    Fixing some bugs

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -8,9 +8,18 @@
         <link rel="canonical" href="{{ $page->getUrl() }}">
         <meta name="description" content="{{ $page->description }}">
         <title>{{ $page->title }}</title>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&family=Work+Sans:wght@700&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
+        <meta property="og:image" content="/assets/images/og-image.png">
+        <meta property="twitter:card" content="summary_large_image">
+        <meta property="og:url" content="{{ $page->getUrl() }}"/>
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="{{ $page->title }}" />
     </head>
     <body>
         @yield('body')
+
+
+        <script src="{{ mix('js/main.js', 'assets/build') }}"></script>
     </body>
 </html>
